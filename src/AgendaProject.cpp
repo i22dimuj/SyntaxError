2d0e7d8c3fe57c9732c7412d5269f140244847d3
Jorge Díaz Muñoz <i22dimuj@uco.es>
2014-12-04 14:04:40 +0100

    Creación del projecto

---
+++ src/AgendaProject.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : AgendaProject.cpp
+// Author      : Jorge Díaz
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
