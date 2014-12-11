71ca38929c471d214e6b07759845dd79f758b4d1
Jorge Díaz Muñoz <i22dimuj@uco.es>
2014-12-11 14:41:08 +0100

    Realizadas algunas modificiones

--- src/AgendaProject.hpp
+++ src/AgendaProject.hpp
@@ -1,10 +1,22 @@
 #ifndef AGENDAPROJECT_HPP
 #define AGENDAPROJECT_HPP
 
+#include <iostream>
+#include <Contacto.hpp>
+#include <list>
+#include <string>
+
 class Agenda
 {
-	private:
+private:
+	std::list <Contacto> _pacientes;
 
-	public:
+public:
+	std::list <Contacto> buscar(std::string apellido);
+	bool insertarContacto(Contacto contacto);
+	bool eliminarContacto(std::string apellido);
+	bool modificarContacto(std::string apellido);
+	void guardarFichero(std::list <Contacto> _paciente);
+
 };
 #endif
