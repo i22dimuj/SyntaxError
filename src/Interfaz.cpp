df2c21d2f45977e5782044e9268bccce1c9340a4
Jorge Díaz <i22dimuj@uco.es>
2014-12-13 13:48:22 +0100

    interfaz

---
+++ src/Interfaz.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Interfaz.hpp"
+
+using namespace std;
+
+class InterfazAgenda
+{
+	int menu()
+	{
+		int seleccion;
+
+		while (int salir == 0)
+		{
+			cout << "----- Agenda -----" << endl << endl;
+			cout << "Menu" << endl;
+			/*
+			 * menu
+			 */
+			return seleccion;
+		}
+	}
+};
