254f24cb945cd9c3aed1c931474d0eac68394f02
Ivan <ivan@Pepino.(none)>
2014-12-31 14:59:50 +0100

    Nuevos Cambios

--- src/Estructuras.hpp
+++ src/Estructuras.hpp
@@ -2,6 +2,7 @@
 #define ESTRUCTURAS_HPP
 
 #include <string>
+#include <iostream>
 
 namespace agenda {
 
@@ -22,5 +23,26 @@ struct direccionPostal {
 	std::string puerta;
 	int codigoPostal;
 };
+
+
+void imprimeRedSocial(redSocial red){
+
+	std::cout << "twitter: " << red.twitter << endl;
+	std::cout << "facebook: " << red.facebook << endl;
+	std::cout << "gPlus: " << red.gPlus << endl;
+	std::cout << "url: " << red.url << endl;
+}
+
+void imprimeDireccionPostal(direccionPostal dir){
+
+	std::cout << "Municipio: " << dir.municipio << endl;
+	std::cout << "Provincia: " << dir.provincia << endl;
+	std::cout << "Calle: " << dir.calle << endl;
+	std::cout << "Portal: " << dir.portal << endl;
+	std::cout << "Piso : " << dir.piso << endl;
+	std::cout << "Puerta: " << dir.puerta << endl;
+	std::cout << "Codigo Postal : " << dir.codigoPostal << endl;
+
+}
 }
 #endif
